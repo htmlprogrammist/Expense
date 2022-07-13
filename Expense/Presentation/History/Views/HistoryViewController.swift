0b5078ed6790d7bfeb4f7e7ecb95910b0076219f
Egor Badmaev <badmaeve2511@gmail.com>
2022-07-13 03:52:57 +0300

    refactor: remove to using `UITableViewController`

--- Expense/Presentation/History/Views/HistoryViewController.swift
+++ Expense/Presentation/History/Views/HistoryViewController.swift
@@ -7,11 +7,18 @@
 
 import UIKit
 
-final class HistoryViewController: UIViewController {
+final class HistoryViewController: UITableViewController {
     // MARK: - Views
     private lazy var filterButtonItem = UIBarButtonItem(image: Images.History.filter, style: .plain, target: self, action: #selector(chooseFilter))
     private lazy var highlightedFilterButtonItem = UIBarButtonItem(image: Images.History.filterHighlighted, style: .plain, target: self, action: #selector(chooseFilter))
     
+    private lazy var segmentedControlBackground: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemGroupedBackground
+        view.addSubview(segmentedControl)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
     private lazy var segmentedControl: UISegmentedControl = {
         let segmentedControl = UISegmentedControl(items: [Texts.History.day, Texts.History.week, Texts.History.month, Texts.History.year])
         segmentedControl.selectedSegmentIndex = 0
@@ -20,26 +27,17 @@ final class HistoryViewController: UIViewController {
         return segmentedControl
     }()
     
-    private lazy var tableView: UITableView = {
-        let tableView = UITableView(frame: .zero, style: .insetGrouped)
-        tableView.delegate = self
-        tableView.dataSource = self
-        tableView.register(HistoryTableViewHeader.self, forHeaderFooterViewReuseIdentifier: HistoryTableViewHeader.identifier)
-        tableView.sectionHeaderHeight = UITableView.automaticDimension
-        tableView.estimatedSectionHeaderHeight = 44
-        tableView.register(HistoryTableViewCell.self, forCellReuseIdentifier: HistoryTableViewCell.identifier)
-        tableView.rowHeight = UITableView.automaticDimension
-        tableView.estimatedRowHeight = 80
-        tableView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 20, right: 0)
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        return tableView
-    }()
-    
     // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupView()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        view.bringSubviewToFront(segmentedControlBackground)
     }
     
     // MARK: - Private methods
@@ -64,7 +62,6 @@ final class HistoryViewController: UIViewController {
     }
     
     private func setupView() {
-        // General
         view.backgroundColor = .systemGroupedBackground
         title = Texts.History.title
         navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addTransaction))
@@ -73,27 +70,32 @@ final class HistoryViewController: UIViewController {
             UIBarButtonItem(image: Images.History.export, style: .plain, target: self, action: #selector(exportHistory))
         ]
         
-        // Adding and setuping subviews
-        view.addSubview(segmentedControl)
-        view.addSubview(tableView)
+//        view.addSubview(tableView)
+        tableView = UITableView(frame: .zero, style: .insetGrouped)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(HistoryTableViewHeader.self, forHeaderFooterViewReuseIdentifier: HistoryTableViewHeader.identifier)
+        tableView.sectionHeaderHeight = UITableView.automaticDimension
+        tableView.estimatedSectionHeaderHeight = 44
+        tableView.register(HistoryTableViewCell.self, forCellReuseIdentifier: HistoryTableViewCell.identifier)
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 80
+        tableView.contentInset = UIEdgeInsets(top: 32, left: 0, bottom: 16, right: 0)
         
-        // Activating constraints
+        view.addSubview(segmentedControlBackground)
         NSLayoutConstraint.activate([
-            segmentedControl.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
-            segmentedControl.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
-            segmentedControl.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
-            
-            tableView.topAnchor.constraint(equalTo: segmentedControl.bottomAnchor, constant: 16),
-            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
-            tableView.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor)
+            segmentedControlBackground.heightAnchor.constraint(equalTo: segmentedControl.heightAnchor),
+            segmentedControlBackground.widthAnchor.constraint(equalTo: segmentedControl.widthAnchor)
+//            segmentedControl.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+//            segmentedControl.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+//            segmentedControl.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
         ])
     }
 }
 
 // MARK: - UITableView
-extension HistoryViewController: UITableViewDataSource, UITableViewDelegate {
-    func numberOfSections(in tableView: UITableView) -> Int {
+extension HistoryViewController {
+    override func numberOfSections(in tableView: UITableView) -> Int {
         // чисто по приколу сделал, по факту надо будет из вьюмодели тягать количество секций
         switch segmentedControl.selectedSegmentIndex {
         case 0:
@@ -107,7 +109,7 @@ extension HistoryViewController: UITableViewDataSource, UITableViewDelegate {
         }
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // чисто по приколу сделал, по факту надо будет из вьюмодели тягать количество транзакций
         switch segmentedControl.selectedSegmentIndex {
         case 0:
@@ -121,7 +123,7 @@ extension HistoryViewController: UITableViewDataSource, UITableViewDelegate {
         }
     }
     
-    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         guard let header = tableView.dequeueReusableHeaderFooterView(withIdentifier: HistoryTableViewHeader.identifier) as? HistoryTableViewHeader
         else {
             fatalError("Could not create header for the table view in History in section \(section)")
@@ -130,7 +132,7 @@ extension HistoryViewController: UITableViewDataSource, UITableViewDelegate {
         return header
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: HistoryTableViewCell.identifier, for: indexPath) as? HistoryTableViewCell
         else {
             fatalError("Could not create cell for the table view in History at indexPath \(indexPath)")
@@ -139,7 +141,7 @@ extension HistoryViewController: UITableViewDataSource, UITableViewDelegate {
         return cell
     }
     
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         
         if segmentedControl.selectedSegmentIndex == 0 {
