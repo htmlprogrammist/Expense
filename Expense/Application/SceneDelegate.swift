2996c3df18e3a9d0f735019452f3a0ca9522083b
Egor Badmaev <badmaeve2511@gmail.com>
2022-08-01 10:50:13 +0300

    fix: begin using Singleton

--- Expense/Application/SceneDelegate.swift
+++ Expense/Application/SceneDelegate.swift
@@ -52,6 +52,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // to restore the scene back to its current state.
         
         // Save changes in the application's managed object context when the application transitions to the background.
-        coordinator?.coreDataManager.saveContext()
+        CoreDataManager.shared.saveContext()
     }
 }
